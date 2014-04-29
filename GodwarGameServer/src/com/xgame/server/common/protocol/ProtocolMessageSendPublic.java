a5bbe58ad8974c4f0e2cf92d2ea6e135ea27c848
李翌文 <johnnyeven@gmail.com>
2014-04-29 23:55:43 +0800

    1

---
+++ GodwarGameServer/src/com/xgame/server/common/protocol/ProtocolMessageSendPublic.java
@@ -0,0 +1,122 @@
+package com.xgame.server.common.protocol;
+
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.UUID;
+import java.util.Map.Entry;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import com.xgame.server.CommandCenter;
+import com.xgame.server.common.CoordinatePair;
+import com.xgame.server.common.PackageItem;
+import com.xgame.server.common.ServerPackage;
+import com.xgame.server.game.ProtocolPackage;
+import com.xgame.server.game.map.Grid;
+import com.xgame.server.game.map.Map;
+import com.xgame.server.network.WorldSession;
+import com.xgame.server.objects.Player;
+import com.xgame.server.objects.WorldObject;
+import com.xgame.server.pool.ServerPackagePool;
+
+public class ProtocolMessageSendPublic implements IProtocol
+{
+	private static Log	log	= LogFactory.getLog( ProtocolCreateGroup.class );
+
+	@Override
+	public void Execute(Object param1, Object param2)
+	{
+		ProtocolPackage parameter = ( ProtocolPackage ) param1;
+		WorldSession session = ( WorldSession ) param2;
+
+		String message = null;
+		long timestamp = Long.MIN_VALUE;
+
+		for ( int i = parameter.offset; i < parameter.receiveDataLength; )
+		{
+			int length = parameter.receiveData.getInt();
+			int type = parameter.receiveData.get();
+			switch ( type )
+			{
+				case EnumProtocol.TYPE_LONG:
+					if ( timestamp == Long.MIN_VALUE )
+					{
+						timestamp = parameter.receiveData.getLong();
+						break;
+					}
+				case EnumProtocol.TYPE_STRING:
+					if ( message == null )
+					{
+						length = parameter.receiveData.getShort();
+						byte[] dst = new byte[length];
+						parameter.receiveData.get( dst );
+						length += 2;
+						try
+						{
+							message = new String( dst, "UTF-8" );
+						}
+						catch ( UnsupportedEncodingException e )
+						{
+							e.printStackTrace();
+						}
+					}
+			}
+			i += ( length + 5 );
+		}
+		log.info( "[MessageSendPublic] RoleId=" + session.getPlayer().roleId );
+		
+		if(message != null && !message.equals(""))
+		{
+
+			// 告诉视野内其他玩家
+			Map m = session.getPlayer().getMap();
+			CoordinatePair coordinate = Map.getCoordinatePair( session
+					.getPlayer().getX(), session.getPlayer().getY() );
+			Grid g = m.getGrid( (int) coordinate.getX(),
+					(int) coordinate.getY() );
+
+			ArrayList< Grid > list = m.getViewGrid( g );
+			Iterator< Grid > itG = list.iterator();
+			Iterator< Entry< UUID, WorldObject >> gridIt;
+			Grid currentGrid;
+			Entry< UUID, WorldObject > en;
+			Player currentPlayer;
+
+			ServerPackage pack = ServerPackagePool.getInstance()
+					.getObject();
+			pack.success = EnumProtocol.ACK_CONFIRM;
+			pack.protocolId = EnumProtocol.MSG_SEND_PUBLIC;
+			
+			String uuid = session.getPlayer().getGuid().toString();
+			pack.parameter.add( new PackageItem( uuid.length(), uuid ) );
+			pack.parameter.add( new PackageItem( message.length(), message ) );
+			
+			while ( itG.hasNext() )
+			{
+				currentGrid = itG.next();
+
+				if ( currentGrid == null )
+				{
+					continue;
+				}
+
+				gridIt = currentGrid.getWorldObjectIterator();
+				while ( gridIt.hasNext() )
+				{
+					en = gridIt.next();
+					
+					currentPlayer = (Player) en.getValue();
+					if ( !currentPlayer.getChannel().isOpen() )
+					{
+						continue;
+					}
+					CommandCenter.send( currentPlayer.getChannel(), pack );
+					ServerPackagePool.getInstance().returnObject( pack );
+				}
+			}
+		}
+	}
+
+}
--- GodwarGameServer/src/com/xgame/server/network/AIOSocketMgr.java
+++ GodwarGameServer/src/com/xgame/server/network/AIOSocketMgr.java
@@ -13,6 +13,7 @@ import com.xgame.server.common.protocol.EnumProtocol;
 import com.xgame.server.common.protocol.ProtocolCreateGroup;
 import com.xgame.server.common.protocol.ProtocolDeleteGroup;
 import com.xgame.server.common.protocol.ProtocolHeartBeat;
+import com.xgame.server.common.protocol.ProtocolMessageSendPublic;
 import com.xgame.server.common.protocol.ProtocolPlayerSelectHero;
 import com.xgame.server.common.protocol.ProtocolRegisterAccountRole;
 import com.xgame.server.common.protocol.ProtocolRequestAccountRole;
@@ -28,57 +29,45 @@ import com.xgame.server.common.protocol.ProtocolShowRoomList;
 import com.xgame.server.common.protocol.ProtocolUpdatePlayerReady;
 import com.xgame.server.common.protocol.ProtocolUpdatePlayerStatus;
 
-public class AIOSocketMgr
-{
-	private AsynchronousServerSocketChannel	server;
-	private AcceptCompletionHandler			acceptHandler;
-	private ReadCompletionHandler			readHandler;
-	private AuthSessionCompletionHandler	authHandler;
-	public final static String				HOST			= "127.0.0.1";
-	public final static int					PORT			= 9050;
-	public static int						counter			= 0;
+public class AIOSocketMgr {
+	private AsynchronousServerSocketChannel server;
+	private AcceptCompletionHandler acceptHandler;
+	private ReadCompletionHandler readHandler;
+	private AuthSessionCompletionHandler authHandler;
+	public final static String HOST = "127.0.0.1";
+	public final static int PORT = 9050;
+	public static int counter = 0;
 
-	private static AIOSocketMgr				instance		= null;
-	private static boolean					allowInstance	= false;
-	private static Log						log				= LogFactory
-																	.getLog( AIOSocketMgr.class );
+	private static AIOSocketMgr instance = null;
+	private static boolean allowInstance = false;
+	private static Log log = LogFactory.getLog(AIOSocketMgr.class);
 
-	public AIOSocketMgr() throws Exception
-	{
-		if ( !allowInstance )
-		{
+	public AIOSocketMgr() throws Exception {
+		if (!allowInstance) {
 			throw new Exception();
 		}
-		try
-		{
+		try {
 			AsynchronousChannelGroup resourceGroup = AsynchronousChannelGroup
-					.withCachedThreadPool( Executors.newCachedThreadPool(), 8 );
-			server = AsynchronousServerSocketChannel.open( resourceGroup );
-			server.bind( new InetSocketAddress( HOST, PORT ), 100 );
+					.withCachedThreadPool(Executors.newCachedThreadPool(), 8);
+			server = AsynchronousServerSocketChannel.open(resourceGroup);
+			server.bind(new InetSocketAddress(HOST, PORT), 100);
 
 			acceptHandler = new AcceptCompletionHandler();
 			readHandler = new ReadCompletionHandler();
 			authHandler = new AuthSessionCompletionHandler();
 
 			bindProtocol();
-		}
-		catch ( IOException e )
-		{
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
 
-	public static AIOSocketMgr getInstance()
-	{
-		if ( instance == null )
-		{
+	public static AIOSocketMgr getInstance() {
+		if (instance == null) {
 			allowInstance = true;
-			try
-			{
+			try {
 				instance = new AIOSocketMgr();
-			}
-			catch ( Exception e )
-			{
+			} catch (Exception e) {
 				e.printStackTrace();
 			}
 			allowInstance = false;
@@ -86,89 +75,75 @@ public class AIOSocketMgr
 		return instance;
 	}
 
-	private void bindProtocol()
-	{
-		ProtocolRouter.getInstance().Bind( EnumProtocol.BASE_UPDATE_STATUS,
-				ProtocolUpdatePlayerStatus.class );
-		ProtocolRouter.getInstance().Bind( EnumProtocol.REQUEST_FIND_PATH,
-				ProtocolRequestFindPath.class );
-		ProtocolRouter.getInstance().Bind( EnumProtocol.INFO_HEART_BEAT,
-				ProtocolHeartBeat.class );
-		ProtocolRouter.getInstance().Bind( EnumProtocol.REQUEST_ACCOUNT_ROLE,
-				ProtocolRequestAccountRole.class );
-		ProtocolRouter.getInstance().Bind( EnumProtocol.REGISTER_ACCOUNT_ROLE,
-				ProtocolRegisterAccountRole.class );
-		ProtocolRouter.getInstance().Bind( EnumProtocol.HALL_SHOW_ROOM_LIST,
-				ProtocolShowRoomList.class );
-		ProtocolRouter.getInstance().Bind( EnumProtocol.HALL_REQUEST_ROOM,
-				ProtocolRequestRoom.class );
-		ProtocolRouter.getInstance().Bind(
-				EnumProtocol.HALL_REQUEST_ENTER_ROOM,
-				ProtocolRequestEnterRoom.class );
-		ProtocolRouter.getInstance().Bind( EnumProtocol.INFO_CREATE_GROUP,
-				ProtocolCreateGroup.class );
-		ProtocolRouter.getInstance().Bind( EnumProtocol.INFO_DELETE_GROUP,
-				ProtocolDeleteGroup.class );
-		ProtocolRouter.getInstance().Bind( EnumProtocol.INFO_SAVE_CARD_GROUP,
-				ProtocolSaveCardConfig.class );
-		ProtocolRouter.getInstance().Bind(
-				EnumProtocol.INFO_REQUEST_CARD_GROUP,
-				ProtocolRequestCardGroup.class );
-		ProtocolRouter.getInstance().Bind( EnumProtocol.INFO_REQUEST_CARD_LIST,
-				ProtocolRequestCardList.class );
+	private void bindProtocol() {
+		ProtocolRouter.getInstance().Bind(EnumProtocol.BASE_UPDATE_STATUS,
+				ProtocolUpdatePlayerStatus.class);
+		ProtocolRouter.getInstance().Bind(EnumProtocol.REQUEST_FIND_PATH,
+				ProtocolRequestFindPath.class);
+		ProtocolRouter.getInstance().Bind(EnumProtocol.INFO_HEART_BEAT,
+				ProtocolHeartBeat.class);
+		ProtocolRouter.getInstance().Bind(EnumProtocol.REQUEST_ACCOUNT_ROLE,
+				ProtocolRequestAccountRole.class);
+		ProtocolRouter.getInstance().Bind(EnumProtocol.REGISTER_ACCOUNT_ROLE,
+				ProtocolRegisterAccountRole.class);
+		ProtocolRouter.getInstance().Bind(EnumProtocol.HALL_SHOW_ROOM_LIST,
+				ProtocolShowRoomList.class);
+		ProtocolRouter.getInstance().Bind(EnumProtocol.HALL_REQUEST_ROOM,
+				ProtocolRequestRoom.class);
+		ProtocolRouter.getInstance().Bind(EnumProtocol.HALL_REQUEST_ENTER_ROOM,
+				ProtocolRequestEnterRoom.class);
+		ProtocolRouter.getInstance().Bind(EnumProtocol.INFO_CREATE_GROUP,
+				ProtocolCreateGroup.class);
+		ProtocolRouter.getInstance().Bind(EnumProtocol.INFO_DELETE_GROUP,
+				ProtocolDeleteGroup.class);
+		ProtocolRouter.getInstance().Bind(EnumProtocol.INFO_SAVE_CARD_GROUP,
+				ProtocolSaveCardConfig.class);
+		ProtocolRouter.getInstance().Bind(EnumProtocol.INFO_REQUEST_CARD_GROUP,
+				ProtocolRequestCardGroup.class);
+		ProtocolRouter.getInstance().Bind(EnumProtocol.INFO_REQUEST_CARD_LIST,
+				ProtocolRequestCardList.class);
 		ProtocolRouter.getInstance().Bind(
 				EnumProtocol.BATTLEROOM_PLAYER_SELECTED_HERO,
-				ProtocolPlayerSelectHero.class );
-		ProtocolRouter.getInstance().Bind(
-				EnumProtocol.BATTLEROOM_PLAYER_READY,
-				ProtocolUpdatePlayerReady.class );
+				ProtocolPlayerSelectHero.class);
+		ProtocolRouter.getInstance().Bind(EnumProtocol.BATTLEROOM_PLAYER_READY,
+				ProtocolUpdatePlayerReady.class);
+		ProtocolRouter.getInstance().Bind(EnumProtocol.MSG_SEND_PUBLIC,
+				ProtocolMessageSendPublic.class);
 	}
 
-	public void startCompletionPort()
-	{
-		server.accept( this, acceptHandler );
-		log.info( "服务器已启动" );
+	public void startCompletionPort() {
+		server.accept(this, acceptHandler);
+		log.info("服务器已启动");
 
-		try
-		{
+		try {
 			System.in.read();
-		}
-		catch ( IOException e )
-		{
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
 
-	public void stopCompletionPort()
-	{
-		try
-		{
+	public void stopCompletionPort() {
+		try {
 			server.close();
-			log.info( "游戏服务器已关闭" );
-		}
-		catch ( IOException e )
-		{
+			log.info("游戏服务器已关闭");
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
 
-	public AsynchronousServerSocketChannel getServer()
-	{
+	public AsynchronousServerSocketChannel getServer() {
 		return server;
 	}
 
-	public void setServer( AsynchronousServerSocketChannel server )
-	{
+	public void setServer(AsynchronousServerSocketChannel server) {
 		this.server = server;
 	}
 
-	public ReadCompletionHandler getReadHandler()
-	{
+	public ReadCompletionHandler getReadHandler() {
 		return readHandler;
 	}
 
-	public AuthSessionCompletionHandler getAuthHandler()
-	{
+	public AuthSessionCompletionHandler getAuthHandler() {
 		return authHandler;
 	}
 }
